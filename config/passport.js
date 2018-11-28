9a8d6d7b92c42952bb180cb6e63c34da2244f112
Paul Briars <38017937+pwalo@users.noreply.github.com>
2018-11-28 00:03:20 +0000

    added message css, prepd deletion js

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ module.exports = function(passport){
         User.findOne(query, function(err, user){
             if(err) throw err; 
             if(!user){
-                return done(null, false, {message: 'Incorrect user or Password', type: 'danger'});
+                return done(null, false, {message: 'Incorrect user or Password'});
                 
             }
             // Match Password
@@ -19,7 +19,7 @@ module.exports = function(passport){
                 if(isMatch){
                     return done(null, user); 
                 } else {
-                    return done(null, false, {message: 'Incorrect user or Password', type: 'danger'}); 
+                    return done(null, false, {message: 'Incorrect user or Password'}); 
                 } 
             }); 
         });
