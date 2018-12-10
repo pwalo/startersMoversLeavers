2b14b31d4fab3434bf5b73efb785cd57c29ad6a8
Paul Briars <38017937+pwalo@users.noreply.github.com>
2018-12-10 21:16:41 +0000

    switch db location - testing only

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database:'mongodb://160.211.133.126:27017/employees',
-    //database:'mongodb://localhost:27017/employees',
+    //database:'mongodb://160.211.133.126:27017/employees',
+    database:'mongodb://localhost:27017/employees',
     secret:'yoursecret'
 }
